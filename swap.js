aef5be1c6a81f239db72f47bfe50d6d70f7f7b84
Vishal Rajput <vishal.rajput23@gmail.com>
2024-02-16 22:39:11 +0530

    Final

---
+++ swap.js
@@ -0,0 +1,11 @@
+let x = 52;
+let y = 107;
+
+console.log(`First number is ${x}`);
+console.log(`Second number is ${y}`);
+let temp = x;
+x = y;
+y = temp;
+console.log(`After swapping:`);
+console.log(`First number is ${x}`);
+console.log(`Second number is ${y}`);
