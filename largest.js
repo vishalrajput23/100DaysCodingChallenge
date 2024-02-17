9d8491d7659b575a4031f8bdda1b32ae6e6b2270
Vishal Rajput <vishal.rajput23@gmail.com>
2024-02-17 18:01:00 +0530

    Final

---
+++ largest.js
@@ -0,0 +1,17 @@
+let x = 3;
+let y = 4;
+let z = 5;
+
+if (x > y && x > z)
+{
+    console.log(`${x} is greatest number`);
+}
+
+if (y > x && y > z)
+{
+    console.log(`${y} is greatest number`);
+}
+else
+{
+    console.log(`${z} is greatest number`);
+}
