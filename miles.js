82ddf1b74617ecd13f1778a256ce070465085841
Vishal Rajput <vishal.rajput23@gmail.com>
2024-02-19 18:22:05 +0530

    Final

---
+++ miles.js
@@ -0,0 +1,3 @@
+let x = 10;                                                 //Distance in KM
+let y = x * 0.621;                                          //Distance in Miles
+console.log(`The miles conversion is ${y}`);
