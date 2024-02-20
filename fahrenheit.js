4552d614ffd0187623864cfbc97470fc44c13574
Vishal Rajput <vishal.rajput23@gmail.com>
2024-02-20 19:07:49 +0530

    Final

---
+++ fahrenheit.js
@@ -0,0 +1,3 @@
+let x = 0;                                                  //Temperature in Celsius
+let y = (9/5) * x + 32;                                     //Temperature in Fahrenheit
+console.log(`Temperature in Fahrenheit is ${y}°F`);
