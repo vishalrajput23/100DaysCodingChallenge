f5816ef9d3f0b91afb6afd928e2e85c7a10ebfc9
Vishal Rajput <vishal.rajput23@gmail.com>
2024-05-11 09:53:47 +0530

    Final

---
+++ sumElem.js
@@ -0,0 +1,8 @@
+
+let arr = [1, 2, 3, 4, 5]                                    //Prints sum of array elements
+let sum = 0;
+for (let i = 0; i < arr.length; i++)
+{
+    sum += arr[i];
+}
+console.log(sum);
