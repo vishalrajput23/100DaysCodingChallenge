8fd2a6827012c043e87ea2625a313d0560fdb088
Vishal Rajput <vishal.rajput23@gmail.com>
2024-05-13 10:18:12 +0530

    Final

---
+++ arrElem.js
@@ -0,0 +1,8 @@
+
+let arr = [1, 2, 3, 4, 5];                                       //Prints array elements
+let str = "";
+for (let i = 0; i < arr.length; i++)
+{
+    str += arr[i]+ " ";
+}
+console.log(str);
