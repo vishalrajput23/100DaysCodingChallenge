f80894dfcc07fb4a751593aa6a55b4ff9ce9d60f
Vishal Rajput <vishal.rajput23@gmail.com>
2024-05-24 08:45:44 +0530

    Final

---
+++ removeSpace.js
@@ -0,0 +1,5 @@
+                                                                      
+let str = "geeks  for geeks";                                        //Removes spaces in string
+let newStr = str.replace(/\s+/g, '');
+console.log(newStr);
+
