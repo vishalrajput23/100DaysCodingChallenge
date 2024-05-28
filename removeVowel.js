b1f60621b522e4591cd0894e8cb6486babded132
Vishal Rajput <vishal.rajput23@gmail.com>
2024-05-28 20:14:25 +0530

    Final

---
+++ removeVowel.js
@@ -0,0 +1,4 @@
+                                                                      
+let str = "what is your name ?";                            //Removes vowels from string
+console.log(str.replace(/[aeiou]/g, ''));
+
